ab9c864770255e6f4ea8979e048d2e9b3ecdd61f
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2014-01-21 09:51:24 +0100

    Improved data retrieval

--- shared.js
+++ shared.js
@@ -1,37 +1,36 @@
 String.prototype.appendVisibleNode = function(node, elementName) {
-    return this + '\n  <' + node + '>' + $("[name^='" + elementName + "']:visible").val() + '</' + node + '>'
+	return this + '\n  <' + node + '>' + $("[name^='" + elementName + "']:visible").val() + '</' + node + '>'
 };
 
 String.prototype.appendNode = function(node, elementName) {
-    return this + '\n  <' + node + '>' + $("[name='" + elementName + "']").val() + '</' + node + '>'
+	return this + '\n  <' + node + '>' + $("[name='" + elementName + "']").val() + '</' + node + '>'
 };
 
 String.prototype.appendNodeWithValue = function(node, value) {
-    return this + '\n  <' + node + '>' + value + '</' + node + '>'
+	return this + '\n  <' + node + '>' + value + '</' + node + '>'
 };
 
+function getDataByName(name) {
+	return $.getJSON('http://www.omdbapi.com/?t=' + name, function(data) { return data });
+}
+
 function getSeries() {
-	var series = getSeriesName()
-	return $.getJSON('http://www.omdbapi.com/?t=' + series, function(data) { return data });
+	var series = getSeriesName();
+	return getDataByName(series);
 }
 
 function getEpisode() {
 	var imdb = $("[name=IMDB_ID]").val();
 	
 	if (imdb != '') {   
-		return $.getJSON('http://omdbapi.com/?plot=full&i=' + imdb, function(data) { return data });		
+		return $.getJSON('http://omdbapi.com/?plot=full&i=' + imdb, function(data) { return data });
+	}
+	
+	imdb =  $("[name^='EpisodeName_']:visible").val();
+	
+	if (imdb != '') {   
+		return getDataByName(imdb);
 	}
 	
 	return undefined;
 }
-
-
-
-
-
-
-chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-    if (msg.action && (msg.action == "createXML")) {
-        createXML();
-    }
-});
